9817645ab6c8eca0d122ef02433c4cdebe165860
chami212 <hicham.akarkach@outlook.fr>
2025-11-06 18:21:23 +0100

    ajout fonctionnalité delete-task

--- delete-task.js
+++
@@ -1,7 +0,0 @@
-// feature/delete-task
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete-btn')) {
-    const li = e.target.parentElement;
-    li.remove();
-  }
-});
